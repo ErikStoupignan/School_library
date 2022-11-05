7a61ca7a01b81ce9e9f006b71f8b8cb3362bb0c4
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-11-04 23:54:31 -0600

    Add solution for the new project

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App
       puts "[ List of books is empty ]\n\n"
     else
       @books.each_with_index do |element, index|
-        puts "#{index + 1}: #{element}"
+        puts "#{index + 1}: #{element.title} by #{element.author}"
       end
       puts "\n"
     end
@@ -71,13 +71,11 @@ class App
     classroom = gets.chomp
     puts 'Age:'
     age = gets.chomp
-    puts 'Parent permission [true/false]'
-    parent_permission = gets.chomp
 
-    new_student = Students.new(age, classroom, name, parent_permission)
+    new_student = Students.new(age, classroom, name)
 
     puts 'New student created successfully'
-    puts "Name: #{name}, Classroom: #{Classroom}, Age: #{age}, Parent permission: #{parent_permission}"
+    puts "Name: #{name}, Classroom: #{Classroom}, Age: #{age}"
 
     @people << new_student
     replay
@@ -85,17 +83,99 @@ class App
 
   def create_a_book
     puts " >> Create a book\n"
-    puts "\ny   [List of books Empty]   "
+    puts 'Add the title:'
+    title = gets.chomp
+    puts 'Add the author:'
+    author = gets.chomp
+
+    new_book = Book.new(title, author)
+
+    puts 'New books created successfully'
+    puts "Title: #{title}, author: #{author}"
+
+    @books << new_book
     replay
   end
 
   def create_a_rental
-    puts " >> Create a rentsl\n"
+    puts " >> Create a rental\n"
+
+    select_a_book
+    book_selected = gets.chomp.to_i
+    puts @books[book_selected - 1].title
+    book = @books[book_selected - 1]
+
+    select_a_person
+    user_selected = gets.chomp.to_i
+    puts @people[user_selected - 1].name
+    person = @people[user_selected - 1]
+
+    puts "\n\n + Add the date: [MM, DD, YYYY]"
+    date = gets.chomp
+
+    new_rental = Rental.new(date, book, person)
+
+    puts 'New rental created successfully'
+    puts "Date: #{date}, Book: #{book.title} by #{person.name}"
+
+    @rentals << new_rental
     replay
+  end
+
+  def select_a_book
+    puts 'Select the book:'
+    if @books.empty?
+      puts "   [ List of books is empty   ]\n"
+      puts '   [ Please, add a book first ]'
+      replay
+    else
+      puts '------------------------------------'
+      @books.each_with_index do |book, index|
+        puts "#{index + 1} : #{book.title} by #{book.author}"
+      end
+      puts '------------------------------------'
+      puts "\n + Selected book:\n"
+    end
+  end
+
+  def select_a_person
+    puts "\n\n\nSelect the user:"
+    if @people.empty?
+      puts "   [ List of people is empty         ]\n"
+      puts '   [ Please, register a person first ]'
+      replay
+    else
+      puts '------------------------------------'
+      @people.each_with_index do |people, index|
+        puts "#{index + 1} : #{people.name}"
+      end
+      puts '------------------------------------'
+      puts "\n + Selected user:\n"
+    end
   end
 
   def list_all_rentals
     puts " >> list_all_rentals\n"
+    if @rentals.empty?
+      puts "[ List of rentals is empty ]\n\n"
+    else
+
+      puts '[ id  ]|[      USER NAME      ]'
+
+      @rentals.each_with_index do |element, index|
+        puts "[  #{index + 1}  ]|    #{element.person.name}"
+      end
+
+      puts "\n\n Select the person id:"
+      id = gets.chomp.to_i
+
+      puts "The rental selected was\n"
+      puts "ID: #{id} - Date: #{@rentals[id - 1].date}
+        Book title: #{@rentals[id - 1].book.title}
+        Author: #{@rentals[id - 1].book.author}
+        User: #{@rentals[id - 1].person.name}
+        Age: #{@rentals[id - 1].person.age}"
+    end
     replay
   end
 end
